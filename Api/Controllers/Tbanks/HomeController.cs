75fb12d0391227ccffa03f90692f037c8ffe77e4
nathalialauver <nathalialauver470@gmail.com>
2025-02-26 20:23:10 -0300

    'testemerge'

--- Api/Controllers/Tbanks/HomeController.cs
+++ Api/Controllers/Tbanks/HomeController.cs
@@ -43,7 +43,7 @@ namespace Api.Controllers.Tbanks
             }
             else
             {
-                 msg = "usuario invalidoo";
+                 msg = "usuario invalidoooooooooo";
             }
 
            // var Tok = new AccountTokenViewModel { Token = Guid.NewGuid(), TokenRefresh = Guid.NewGuid() };
